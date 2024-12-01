953ca5df6c82b8a6ee0963cb8f38d655dccfda26
Özgür Işık <ozgur.isk@icloud.com>
2024-12-01 19:24:35 +0300

    service layer added

---
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/luv2code/cruddemo/service/EmployeeService.java
@@ -0,0 +1,11 @@
+package com.luv2code.cruddemo.service;
+
+import com.luv2code.cruddemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    List<Employee> findAll();
+
+}
