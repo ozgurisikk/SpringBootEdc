35ac8d3b5e38307af3019a48353c042c472fb550
Özgür Işık <ozgur.isk@icloud.com>
2024-12-01 14:44:16 +0300

    dao part added

---
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/luv2code/cruddemo/rest/EmployeeRestController.java
@@ -0,0 +1,25 @@
+package com.luv2code.cruddemo.rest;
+
+import com.luv2code.cruddemo.dao.EmployeeDAO;
+import com.luv2code.cruddemo.entity.Employee;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+
+    private EmployeeDAO employeeDAO;
+
+    public EmployeeRestController(EmployeeDAO theEmployeeDAO) {
+        this.employeeDAO = theEmployeeDAO;
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> findAll(){
+        return employeeDAO.findAll();
+    }
+}
