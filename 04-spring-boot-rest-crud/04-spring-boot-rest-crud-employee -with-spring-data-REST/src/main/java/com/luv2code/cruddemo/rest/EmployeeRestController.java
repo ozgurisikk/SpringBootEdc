c3ac353516845971c878a0243cfa3e99651810f0
Özgür Işık <ozgur.isk@icloud.com>
2024-12-02 14:22:45 +0300

    Spring Data Rest configs done before deleting service and rest packages.

---
+++ 04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee -with-spring-data-REST/src/main/java/com/luv2code/cruddemo/rest/EmployeeRestController.java
@@ -0,0 +1,57 @@
+package com.luv2code.cruddemo.rest;
+
+import com.luv2code.cruddemo.entity.Employee;
+import com.luv2code.cruddemo.service.EmployeeService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+
+    private EmployeeService employeeService;
+
+    public EmployeeRestController(EmployeeService theEmployeeService) {
+        this.employeeService = theEmployeeService;
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> findAll(){
+        return employeeService.findAll();
+    }
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee findById(@PathVariable int employeeId){
+        Employee theEmployee = employeeService.findById(employeeId);
+        if (theEmployee == null){
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+        return theEmployee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee theEmployee){
+        theEmployee.setId(0);
+        Employee dbEmployee = employeeService.save(theEmployee);
+        return dbEmployee;
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee theEmployee){
+        Employee dbEmployee = employeeService.save(theEmployee);
+        return dbEmployee;
+    }
+
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId){
+        Employee dbEmployee = employeeService.findById(employeeId);
+        if (dbEmployee == null ){
+            throw new RuntimeException("Employee not found");
+        }
+
+        employeeService.deleteById(employeeId);
+
+        return "Deleted employee: " + dbEmployee;
+    }
+}
