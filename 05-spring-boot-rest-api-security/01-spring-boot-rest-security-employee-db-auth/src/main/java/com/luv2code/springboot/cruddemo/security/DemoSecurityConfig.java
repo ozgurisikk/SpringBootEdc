4d82842a3fa8970d6179adc72a2a554c29db17f4
Özgür Işık <ozgur.isk@icloud.com>
2024-12-04 16:04:12 +0300

    For Spring Boot REST API Security adding security support using JPA/Hibernate

--- 05-spring-boot-rest-api-security/01-spring-boot-rest-security-employee-db-auth/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ 05-spring-boot-rest-api-security/01-spring-boot-rest-security-employee-db-auth/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -22,7 +22,19 @@ public class DemoSecurityConfig {
     @Bean
     public UserDetailsManager userDetailsManager (DataSource dataSource){
 
-        return new JdbcUserDetailsManager(dataSource);
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+        // define query to retrieve a user by username
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+                "SELECT user_id, pw, active FROM members where user_id=?"
+        );
+
+        // define query to retrieve the authorities / roles by username
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+                "SELECT user_id, role FROM roles WHERE user_id=?"
+        );
+
+        return jdbcUserDetailsManager;
         // tells Spring Security to use JDBC Authentication with our data source
 
     }
