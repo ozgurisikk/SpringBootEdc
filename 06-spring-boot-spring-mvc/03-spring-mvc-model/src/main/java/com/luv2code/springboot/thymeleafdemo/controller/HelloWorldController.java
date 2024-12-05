410d3a3b4dadab7c524695c9d4387ab0d3c5a4fa
Özgür Işık <ozgur.isk@icloud.com>
2024-12-05 22:20:22 +0300

    Added data to model

--- 06-spring-boot-spring-mvc/03-spring-mvc-model/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ 06-spring-boot-spring-mvc/03-spring-mvc-model/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -1,6 +1,8 @@
 package com.luv2code.springboot.thymeleafdemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -16,6 +18,18 @@ public class HelloWorldController {
 
     @RequestMapping("/processForm")
     public String processForm(){
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormTwo")
+    public String shout(HttpServletRequest request, Model theModel){
+
+        String name = request.getParameter("studentName");
+
+        String result = "YO! " + name.toUpperCase();
+
+        theModel.addAttribute("message", result);
+
         return "helloworld";
     }
 
