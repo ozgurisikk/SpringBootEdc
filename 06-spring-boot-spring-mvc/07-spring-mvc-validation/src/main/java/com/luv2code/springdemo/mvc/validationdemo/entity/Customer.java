f6e2006cda6982234705a4da17a405cdd28e1aef
Özgür Işık <ozgur.isk@icloud.com>
2024-12-07 13:44:21 +0300

    Spring MVC Pattern - regexp validation added

--- 06-spring-boot-spring-mvc/07-spring-mvc-validation/src/main/java/com/luv2code/springdemo/mvc/validationdemo/entity/Customer.java
+++ 06-spring-boot-spring-mvc/07-spring-mvc-validation/src/main/java/com/luv2code/springdemo/mvc/validationdemo/entity/Customer.java
@@ -1,9 +1,6 @@
 package com.luv2code.springdemo.mvc.validationdemo.entity;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public class Customer {
 
@@ -15,6 +12,9 @@ public class Customer {
     @Min(value = 0 , message = "Must be equal to zero or higher")
     @Max(value = 10, message = "Must be less than 10")
     private int freePasses;
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}" , message = "only 5 chars/digits allowed")
+    private String postalCode;
 
     public String getFirstName() {
         return firstName;
@@ -38,5 +38,13 @@ public class Customer {
 
     public void setFreePasses(int freePasses) {
         this.freePasses = freePasses;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
     }
 }
