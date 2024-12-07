abf67c59f3b67be7207e566303b9c4e71782213a
Özgür Işık <ozgur.isk@icloud.com>
2024-12-07 13:13:51 +0300

    Spring MVC validation Trimming white spaces with @InitBinder annotation and turning into null added

--- 06-spring-boot-spring-mvc/07-spring-mvc-validation/src/main/java/com/luv2code/springdemo/mvc/validationdemo/controller/CustomerController.java
+++ 06-spring-boot-spring-mvc/07-spring-mvc-validation/src/main/java/com/luv2code/springdemo/mvc/validationdemo/controller/CustomerController.java
@@ -2,15 +2,25 @@ package com.luv2code.springdemo.mvc.validationdemo.controller;
 
 import com.luv2code.springdemo.mvc.validationdemo.entity.Customer;
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
+
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder){
+        StringTrimmerEditor strTrimEdt = new StringTrimmerEditor(true);
+
+        dataBinder.registerCustomEditor(String.class, strTrimEdt);
+    }
 
     @GetMapping("/")
     public String showForm(Model theModel){
@@ -26,6 +36,7 @@ public class CustomerController {
                           BindingResult theBindingResult ){
         //validation results are stored in binding result. You may need to place it just after model parameter
 
+        System.out.println("Last name: |" + theCustomer.getLastName() + "|");
         if(theBindingResult.hasErrors()){
             return "customer-form";
         }else{
