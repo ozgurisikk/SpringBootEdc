a55d1e326570a4c3652e4ce7cbe35a3b3589fd63
Özgür Işık <ozgur.isk@icloud.com>
2024-12-07 13:39:52 +0300

    Spring MVC Min-Max integer validation added

--- 06-spring-boot-spring-mvc/07-spring-mvc-validation/src/main/java/com/luv2code/springdemo/mvc/validationdemo/entity/Customer.java
+++ 06-spring-boot-spring-mvc/07-spring-mvc-validation/src/main/java/com/luv2code/springdemo/mvc/validationdemo/entity/Customer.java
@@ -1,5 +1,7 @@
 package com.luv2code.springdemo.mvc.validationdemo.entity;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -9,6 +11,10 @@ public class Customer {
     @NotNull(message="is required")
     @Size(min=1 , message="is required")
     private String lastName;
+
+    @Min(value = 0 , message = "Must be equal to zero or higher")
+    @Max(value = 10, message = "Must be less than 10")
+    private int freePasses;
 
     public String getFirstName() {
         return firstName;
@@ -24,5 +30,13 @@ public class Customer {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public int getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(int freePasses) {
+        this.freePasses = freePasses;
     }
 }
