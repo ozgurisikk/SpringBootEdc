476e1fa3e19af19306ecd1b08327c97982732592
Özgür Işık <ozgur.isk@icloud.com>
2024-12-09 22:45:09 +0300

    Spring Boot MVC CRUD - employee adding process completed

--- 07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ 07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -32,8 +34,25 @@ public class EmployeeController {
         // add to the spring model
         theModel.addAttribute("employees", employeeList);
 
+        return "employees/list-employees";
+    }
 
-        return "list-employees";
+    @GetMapping("/showFormForAdd")
+    public String showForm(Model theModel){
+        Employee theEmployee = new Employee();
+
+        theModel.addAttribute("employee", theEmployee);
+
+        return "employees/employee-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee theEmployee){
+
+        employeeService.save(theEmployee);
+
+        return "redirect:/employees/list";
+
     }
 
 }
